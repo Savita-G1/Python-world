05d9de9eae64fce8de25272e80e024572f74170f
Savita-G1 <savitagavade546@gmail.com>
2025-07-01 13:14:48 +0530

    add new Python file

---
+++ Task1.py
@@ -0,0 +1,18 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Task 1: Perform Basic Mathematical Operations
+... 
+... # Take input from the user
+... num1 = float(input("Enter the first number: "))
+... num2 = float(input("Enter the second number: "))
+... 
+... # Perform operations
+... addition = num1 + num2
+... subtraction = num1 - num2
+... multiplication = num1 * num2
+... division = num1 / num2
+... 
+... # Display results
+... print("\nAddition:", addition)
+... print("Subtraction:", subtraction)
+... print("Multiplication:", multiplication)
---
+++ Task2.py
@@ -0,0 +1,11 @@
+# Task 2: Create a Personalized Greeting
+
+# Take user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Display personalized greeting
+print(f"\nHello, {full_name}! Welcome to the Python program.")
