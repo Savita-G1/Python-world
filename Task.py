65b5e29220b4b446bfc4a4aa16d9b43985168cad
Savita-G1 <128224748+Savita-G1@users.noreply.github.com>
2025-07-05 11:54:14 +0530

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,16 @@
+ # Task 1: Perform Basic Mathematical Operations
+... # Take input from the user
+... num1 = float(input("Enter the first number: "))
+... num2 = float(input("Enter the second number: "))
+... 
+... # Perform operations
+... addition = num1 + num2
+... subtraction = num1 - num2
+... multiplication = num1 * num2
+... division = num1 / num2
+... 
+... # Display results
+... print("\nAddition:", addition)
+... print("Subtraction:", subtraction)
+... print("Multiplication:", multiplication)
+... print("division:", division)
